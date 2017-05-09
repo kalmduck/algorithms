5c9198e6f241aa40e99d24f74c2f591ae0e2cfc5
Benjamin Strecker <benstrecker@gmail.com>
2017-05-08 21:32:18 -0400

    Basic articulation points search

--- articulation/articulation.go
+++ articulation/articulation.go
@@ -1,35 +1,61 @@
 package articulation
 
-import "github.com/kalmduck/container/graph"
+import (
+	"errors"
+	"log"
+
+	"github.com/kalmduck/container/graph"
+)
 
 // This package implements Tarjan's Algorithm for determining the articulation
 // points (if any) in an undirected graph.
 
-var visited []bool
-var depth []int
-var low []int
-var parent []int
-var ap []int
+type tracker struct {
+	visited []bool
+	depth   []int
+	low     []int
+	parent  []int
+	ap      []int
+}
+
+func newTracker(n int) *tracker {
+	t := &tracker{
+		make([]bool, n),
+		make([]int, n),
+		make([]int, n),
+		make([]int, n),
+		nil,
+	}
+	for i := range t.parent {
+		t.parent[i] = -1
+	}
+	return t
+}
 
 // GetArticulationPoints returns a slice containing the nodes
 // determined to be articulation points.
 func GetArticulationPoints(g *graph.Graph) []int {
-	visited = make([]bool, len(g.Nodes))
-	depth = make([]int, len(g.Nodes))
-	low = make([]int, len(g.Nodes))
-	parent = make([]int, len(g.Nodes))
-	for i := range parent {
-		parent[i] = -1
+	t := newTracker(len(g.Nodes))
+	for !t.allVisited() {
+		n, err := t.nextUnvisited()
+		if err != nil {
+			log.Fatal("Still looping when all have been visited")
+		}
+		findAP(g, t, n, 0)
 	}
-	return ap
+	return t.ap
 }
 
-func findAP(g *graph.Graph, i, d int) {
-	visited[i] = true
-	depth[i] = d
-	low[i] = d
+// findAP performs the actual work of finding the articulation points in a
+// connected graph.
+func findAP(g *graph.Graph, t *tracker, i, d int) {
+	t.visited[i] = true
+	t.depth[i] = d
+	t.low[i] = d
 	childCount := 0
 	isAP := false
+
+	// simple min function for ints.
 	min := func(l, ladj int) int {
 		if l < ladj {
 			return l
@@ -37,19 +63,41 @@ func findAP(g *graph.Graph, i, d int) {
 		return ladj
 	}
 	for _, adj := range g.Nodes[i].Edges {
-		if !visited[adj] {
-			parent[adj] = i
-			findAP(g, adj, d+1)
+		if !t.visited[adj] { // we haven't been hear yet
+			t.parent[adj] = i // mark the parent so we don't go in reverse
+			findAP(g, t, adj, d+1)
 			childCount = childCount + 1
-			if low[adj] >= depth[i] {
+
+			// adj doesn't have any loops to an ancestor
+			if t.low[adj] >= t.depth[i] {
 				isAP = true
 			}
-			low[i] = min(low[i], low[adj])
-		} else if adj != parent[i] {
-			low[i] = min(low[i], depth[adj])
+
+			// update the lowpoint based on this child
+			t.low[i] = min(t.low[i], t.low[adj])
+		} else if adj != t.parent[i] { // already visited, but not the direct parent
+			t.low[i] = min(t.low[i], t.depth[adj])
 		}
 	}
-	if (parent[i] >= 0 && isAP) || (parent[i] == -1 && childCount > 1) {
-		ap = append(ap, i)
+	// non-root and isAP or root and at least 2 children
+	if (t.parent[i] >= 0 && isAP) || (t.parent[i] == -1 && childCount > 1) {
+		t.ap = append(t.ap, i)
 	}
+}
+
+func (t tracker) nextUnvisited() (int, error) {
+	for i, v := range t.visited {
+		if !v {
+			return i, nil
+		}
+	}
+	return -1, errors.New("nextUnvisited() called on tracker with none left to visit")
+}
+
+func (t tracker) allVisited() bool {
+	done := true
+	for _, v := range t.visited {
+		done = done && v
+	}
+	return done
 }
---
+++ articulation/articulation_test.go
@@ -0,0 +1,85 @@
+package articulation
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kalmduck/container/graph"
+)
+
+func TestAllVisited(t *testing.T) {
+	track := newTracker(5)
+	if track.allVisited() {
+		t.Error("allVisited() should have returned false.")
+	}
+	for i := range track.visited {
+		track.visited[i] = true
+	}
+	if !track.allVisited() {
+		t.Error("allVisited() should have returned true.")
+	}
+}
+
+func TestNextUnvisited(t *testing.T) {
+	track := newTracker(5)
+	if n, _ := track.nextUnvisited(); n != 0 {
+		t.Error("Next unvisited wasn't 0")
+	}
+	track.visited[0] = true
+	if n, _ := track.nextUnvisited(); n != 1 {
+		t.Error("Next unvisited wasn't 1")
+	}
+	for i := range track.visited {
+		track.visited[i] = true
+	}
+	if _, err := track.nextUnvisited(); err == nil {
+		t.Error("Should have gotten an error for a fully visited graph")
+	}
+}
+
+func TestVerySimpleGraph(t *testing.T) {
+	g := graph.New(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(1, 2)
+	ap := GetArticulationPoints(g)
+	if len(ap) != 1 {
+		t.Errorf("len(ap): expected: 1, actual: %d\n", len(ap))
+	}
+	if ap[0] != 1 {
+		t.Errorf("expected ap: 1, actual: %d\n", ap[0])
+	}
+}
+
+func TestUnconnectedGraph(t *testing.T) {
+	g := graph.New(6)
+	g.AddEdge(0, 1)
+	g.AddEdge(1, 2)
+	g.AddEdge(3, 4)
+	g.AddEdge(4, 5)
+	ap := GetArticulationPoints(g)
+	if len(ap) != 2 {
+		t.Errorf("expected 2 ap nodes, got: %d", len(ap))
+	}
+	if !contains(ap, 1) {
+		t.Error("ap doesn't have 1")
+	}
+	if !contains(ap, 4) {
+		t.Error("ap doesn't have 4")
+	}
+}
+
+func contains(vals []int, n int) bool {
+	for _, v := range vals {
+		if v == n {
+			return true
+		}
+	}
+	return false
+}
+
+func TestRandomGraph(t *testing.T) {
+	g := graph.NewRandomGraph(100, 50)
+	fmt.Println(g)
+	ap := GetArticulationPoints(g)
+	fmt.Println(ap)
+}
